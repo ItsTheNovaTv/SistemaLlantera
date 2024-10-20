275e8096959974a58598ab4c8c95ba99ae697a95
ItsTheNovaTv <114026464+ItsTheNovaTv@users.noreply.github.com>
2024-10-20 03:43:22 -0700

    python file
    added the code i was working with

---
+++ llantera.py
@@ -0,0 +1,34 @@
+import tkinter as tk
+from tkinter import ttk
+
+window = tk.Tk()
+window.title('Llantera Urias')
+window.geometry('600x400')
+
+
+framelateral = tk.Frame(window, bg='lightblue')
+botonMenu= tk.Button(framelateral, text='Menu', width=10).pack(side='bottom', pady=2)
+
+framelateral.pack(side='left', padx=2,pady=2, fill='y')
+
+framesuperior = tk.Frame(window, bg='orange')
+label1 = ttk.Label(framesuperior, text='Llantera Urias', font=('Arial',16,'bold'),background='orange')
+label1.pack(side='left', fill='both', expand=True)
+boton1 = tk.Button(framelateral, text='Aceptar')
+boton1.pack(side='bottom', fill='x',pady=2)
+boton2= tk.Button(framelateral, text='Aceptar')
+boton2.pack(side='bottom', fill='x',pady=2)
+
+framesuperior.pack(side='top', fill='both', pady=2, padx=2)
+
+framecontenido = tk.Frame(window,bg='lightgreen')
+framecontenido.pack(side='right', fill='both', expand=True,padx=2,pady=2)
+def Menu():
+    botonmenu1 = tk.Button(framecontenido, text='click').pack(side='right', anchor='se')
+
+Menu()
+
+
+
+
+window.mainloop()
